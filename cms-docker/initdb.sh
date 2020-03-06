8ac91e50985398a48fe51ce6e55ee605816e6945
ryanking13 <def6488@gmail.com>
2020-03-06 15:34:51 +0000

    fix syntax errors

--- cms-docker/initdb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-psql --username=cmsuser --dbname=cmsdb --command='ALTER SCHEMA public OWNER TO cmsuser'
-psql --username=cmsuser --dbname=cmsdb --command='GRANT SELECT ON pg_largeobject TO cmsuser'
--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 if [[ "$USER" != "root" ]]; then
 	echo "Usage: sudo ./install.sh"
@@ -19,11 +18,23 @@ which docker >/dev/null
 if [ $? -ne 0 ]; then
     curl -fsSL https://get.docker.com | sh
 fi
-if [ $? -ne docker-compose 0 ]; then
+
+which docker-compose >/dev/null
+if [ $? -ne 0 ]; then
     curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
+docker kill $(docker ps -q)
+
+RANDOM_SECRET=$(hexdump -n 16 -v -e ' 16/1 "%02X"' /dev/urandom)
+RANDOM_DB_PASS=$(hexdump -n 16 -v -e ' 16/1 "%02X"' /dev/urandom)
+sed "s/__CMSPASS__/${RANDOM_DB_PASS}/g" ${PWD}/cms-docker/cms.conf.sample > ${PWD}/cms-docker/cms.conf.bak
+sed "s/123455EC2E767890/${RANDOM_SECRET}/g" ${PWD}/cms-docker/cms.conf.bak > ${PWD}/cms-docker/cms.conf
+rm -f ${PWD}/cms-docker/cms.conf.bak
+sed "s/__CMSPASS__/${RANDOM_DB_PASS}/g" ${PWD}/cms-docker/initdb.sh.sample > ${PWD}/cms-docker/initdb.sh
+chmod +x ${PWD}/cms-docker/initdb.sh
+
 # Run docker
 docker-compose build cms
-docker-compose up cms
+docker-compose up -d
